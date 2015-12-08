8b214575d013029c21db287613203aa0fca7a680
Matt Walker <matt.g.d.walker@gmail.com>
2015-12-07 21:58:37 -0500

    Refactored DynamicDoc
    It now is a `Prcessable DynamicDoc Doc'.  `Processable' is a typeclass
    with some laws, found in the source.  I won't elaborate here.

--- site-haskell/src/XMonad/Hooks/DynamicLog/PrettyPrinter/DynamicDoc.hs
+++ site-haskell/src/XMonad/Hooks/DynamicLog/PrettyPrinter/DynamicDoc.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module XMonad.Hooks.DynamicLog.PrettyPrinter.DynamicDoc where
 
 import XMonad.Util.Run
@@ -10,28 +12,46 @@ import System.IO
 
 type Process = (FilePath, [String])
 
-data DynamicDoc = DynDoc Doc
-                | DynStr String
-                | DynProc Process
-                | CompoundDynDoc [DynamicDoc] deriving (Show, Eq)
+data DynDoc = DynDoc Doc
+            | DynProc Process deriving (Show, Eq)
+
+data DynamicDoc = Atom (DynDoc, Doc -> Doc)
+                | Compound ([DynamicDoc], Doc -> Doc)
+
+-- | Laws:
+-- (i)   process $ f .$. x = (process x) >>= f
+-- (ii)  id .$. x = x
+-- (iii) (f . g) .$. x = f .$. (g .$. x)
+class Processable a b where
+  (.$.)   :: Monoid w => (b -> b) -> w -> w
+  process :: (Monad m, Monoid w) => w -> m b
 
 instance Monoid DynamicDoc where
-  (CompoundDynDoc d) `mappend` (CompoundDynDoc e) = CompoundDynDoc $ d ++ e
-  (CompoundDynDoc d) `mappend` x                  = CompoundDynDoc $ d ++ [x]
-  x `mappend` (CompoundDynDoc d)                  = CompoundDynDoc $ x:d
-  x `mappend` y                                   = CompoundDynDoc $ [x, y]
-  mempty                                          = CompoundDynDoc $ []
+  x `mappend` y = Compound ([x, y], id)
+  mempty        = Compound ([], id)
+
+instance Processable DynamicDoc Doc where
+  f .$. (Atom x)     = Atom     (fst x, f . (snd x))
+  f .$. (Compound x) = Compound (fst x, f . (snd x))
+  process            = compileDynamicDoc
 
 runProcess :: MonadIO m => Process -> m String
 runProcess (process, args) = runProcessWithInput process args ""
 
+compileDynDoc :: MonadIO m => DynDoc -> m Doc
+compileDynDoc (DynDoc d)  = return d
+compileDynDoc (DynProc p) = text <$> runProcess p 
+
 compileDynamicDoc :: MonadIO m => DynamicDoc -> m Doc
-compileDynamicDoc (DynDoc d) = return d
-compileDynamicDoc (DynStr s) = return $ text s
-compileDynamicDoc (DynProc p) = fmap text $ runProcess p
-compileDynamicDoc (CompoundDynDoc cdd) = fmap (foldl (\acc x -> acc <> x) empty) 
-                                     $ sequence
-                                     $ fmap compileDynamicDoc cdd
+compileDynamicDoc (Atom (d, f)) = f <$> compileDynDoc d
+compileDynamicDoc (Compound (ds, f)) = f <$> mconcat <$> sequence $ compileDynamicDoc <$> ds
+
+-- compileDynamicDoc (DynDoc d) = return d
+-- compileDynamicDoc (DynStr s) = return $ text s
+-- compileDynamicDoc (DynProc p) = fmap text $ runProcess p
+-- compileDynamicDoc (CompoundDynDoc cdd) = fmap (foldl (\acc x -> acc <> x) empty) 
+--                                      $ sequence
+--                                      $ fmap compileDynamicDoc cdd
 
 renderStatusBar :: Doc -> String
 renderStatusBar = renderStyle style 
